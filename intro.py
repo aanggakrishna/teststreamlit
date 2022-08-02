9c04acd800bd34e9d06489d5805547afdaccfbe8
aanggakrishna <31291397+aanggakrishna@users.noreply.github.com>
2022-08-02 23:08:25 +0800

    Update intro.py

--- intro.py
+++ intro.py
@@ -4,10 +4,6 @@ import pandas as pd
 st.write("Hellow World")
 "Ini tanpa write"
 
-url='https://drive.google.com/file/d/1BtDpRNj8ha4onbtlSUXMnGsWrt3idqT3/view?usp=sharing'
-file_id=url.split('/')[-2]
-dwn_url='https://drive.google.com/uc?id=' + file_id
-df = pd.read_csv(dwn_url)
-
+df = pd.read_csv("store.csv")
 
 st.table(df)
